2ce74d4eee41d7d92d09a59551c7b0d15270dc38
atrox3d <atrox3d@gmail.com>
2023-01-26 05:44:19 +0100

    improve substitution

--- normalize.sh
+++ normalize.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
-python -c 'import sys;path="-".join(sys.argv[1:]).lower().replace(".-", ".");print(path)' $*
+
+code='
+import sys
+
+DEBUG = False
+def dprint(*args, **kwargs):
+    if not DEBUG:
+        return
+    print("DEBUG | ", *args, **kwargs)
+
+text = sys.argv[1:]
+dprint(text)
+
+text = "-".join(text)
+text = text.lower()
+
+dash = "-"
+to_dash = " ,;:"
+
+dot = "."
+to_dot = [".-"]
+
+substitutions = {
+    dash: to_dash,
+    dot: to_dot
+}
+dprint(substitutions)
+
+for replace, targets in substitutions.items():
+    for target in targets:
+        text = text.replace(target, replace)
+
+while "--" in text:
+    text = text.replace("--", "-")
+
+print(text)
+'
+python -c "$code" $*
